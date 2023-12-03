6ab3019035701d9deac8db9ad5ad62d6049268cb
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-03 15:15:33 +0000

    found symbol

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 
 def findPartNumLoc(c):
-  hx = line.index(c)+1
-  hx_ = line.index(c)-1
-  hy = schema.index(line)+1
-  hy_ = schema.index(line)-1
-  return (hx, hy, hx_, hy_)
+  h0 = line.index(c)
+  h00 = schema.index(line)
+  hx=line.index(c)+1
+  hy=schema.index(line)+1
+  hx_=line.index(c)-1
+  hy_=schema.index(line)-1
+  return h0, h00, hx, hy, hx_, hy_
+
+def findNum(c):
   
 
 f = open("day2_input.txt", "r")
@@ -16,8 +20,14 @@ numbers = [str(i) for i in range(10)]
 for line in schema:
   for c in line:
     if c!="." and c not in numbers:
-      print(f"found a symbol {c} in line {line} ")
-      hx,hy,hx_,hy_ = findPartNumLoc(c)
+      print(f"found a symbol {c} in line {line} at {line.index(c)} in schema line {schema.index(line)} ")
+      h0, h00, hx, hy, hx_, hy_ = findPartNumLoc(c)
+      print(f"h0 = {h0}, h00 = {h00}, hx = {hx}, hy = {hy}, hx_ = {hx_}, hy_ = {hy_}")
+      
+      print(f"should be symbol = {schema[h00][h0]}")
+
+      
       y = input("enter a key: ")
+      
 
 f.close
