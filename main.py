fbf6521638a5736706c3464320a352c7ca0b9dbb
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-05 18:00:57 +0000

    reading data done and changed the data to numbers and tidied up the names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-def readFile2Data()
+def readFile2Data():
   f=open("day5_input.txt", "r")
   
   #reading the file and storing it in a list
@@ -8,39 +8,48 @@ def readFile2Data()
   
   seeds = inData[0].split()
   seeds.remove("seeds:")
+  seeds = [int(i) for i in seeds]
   
-  soilMap = inData[2:inData.index("soil-to-fertilizer map:")]
-  soilMap = [line.split() for line in soilMap]
-  #print(soilMap)
+  seed2SoilMap = inData[2:inData.index("soil-to-fertilizer map:")]
+  seed2SoilMap = [line.split() for line in seed2SoilMap]
+  seed2SoilMap = [[int(i) for i in line] for line in seed2SoilMap]
+  #print(seed2SoilMap)
   
-  fertlMap = inData[inData.index("soil-to-fertilizer map:")+1:inData.index("fertilizer-to-water map:")]
-  fertlMap = [line.split() for line in fertlMap]
-  #print(fertlMap)
+  soil2FertlMap = inData[inData.index("soil-to-fertilizer map:")+1:inData.index("fertilizer-to-water map:")]
+  soil2FertlMap = [line.split() for line in soil2FertlMap]
+  soil2FertlMap = [[int(i) for i in line] for line in soil2FertlMap]
+  #print(soil2FertlMap)
   
-  waterMap = inData[inData.index("fertilizer-to-water map:")+1:inData.index("water-to-light map:")]
-  waterMap = [line.split() for line in waterMap]
-  #print(f"waterMap = {waterMap}")
+  fertl2WaterMap = inData[inData.index("fertilizer-to-water map:")+1:inData.index("water-to-light map:")]
+  fertl2WaterMap = [line.split() for line in fertl2WaterMap]
+  fertl2WaterMap = [[int(i) for i in line] for line in fertl2WaterMap]
+  #print(f"fertl2WaterMap = {fertl2WaterMap}")
   
-  lightmap  = inData[inData.index("water-to-light map:")+1:inData.index("light-to-temperature map:")]
-  lightmap = [line.split() for line in lightmap]
-  #print(f"lightmap = {lightmap}")
+  water2LightMap  = inData[inData.index("water-to-light map:")+1:inData.index("light-to-temperature map:")]
+  water2LightMap = [line.split() for line in water2LightMap]
+  water2LightMap = [[int(i) for i in line] for line in water2LightMap]
   
-  tempMap = inData[inData.index("light-to-temperature map:")+1:inData.index("temperature-to-humidity map:")]
-  tempMap = [line.split() for line in tempMap]
-  #print(f"tempMap = {tempMap}")
+  #print(f"water2LightMap = {water2LightMap}")
   
-  humidMap = inData[inData.index("temperature-to-humidity map:")+1:inData.index("humidity-to-location map:")]
-  humidMap = [line.split() for line in humidMap]
-  #print(f"humidMap = {humidMap}")
+  light2TempMap = inData[inData.index("light-to-temperature map:")+1:inData.index("temperature-to-humidity map:")]
+  light2TempMap = [line.split() for line in light2TempMap]
+  light2TempMap = [[int(i) for i in line] for line in light2TempMap]
+  #print(f"light2TempMap = {light2TempMap}")
   
-  locMap = inData[inData.index("humidity-to-location map:")+1:]
-  locMap = [line.split() for line in locMap]
-  print(f"locMap = {locMap}")
+  temp2HumidMap = inData[inData.index("temperature-to-humidity map:")+1:inData.index("humidity-to-location map:")]
+  temp2HumidMap = [line.split() for line in temp2HumidMap]
+  temp2HumidMap = [[int(i) for i in line] for line in temp2HumidMap]
+  #print(f"temp2HumidMap = {temp2HumidMap}")
   
-  print(f"left the for loop too. ")
+  humid2LocMap = inData[inData.index("humidity-to-location map:")+1:]
+  humid2LocMap = [line.split() for line in humid2LocMap]
+  humid2LocMap = [[int(i) for i in line] for line in humid2LocMap]
+  #print(f"humid2LocMap = {humid2LocMap}")
+
   f.close
-  return(seeds, soilMap, fertlMap, waterMap, lightmap, tempMap, humidMap,)
+  return(seeds, seed2SoilMap, soil2FertlMap, fertl2WaterMap, water2LightMap, light2TempMap, temp2HumidMap,humid2LocMap)
 
-readFile2Data()
+seeds, seed2SoilMap, soil2FertlMap, fertl2WaterMap, water2LightMap, light2TempMap, temp2HumidMap, humid2LocMap =  readFile2Data()
 
+#The range is start+range-1
 
