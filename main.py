ca16c181b27b1331334d2c62d173007112bc04d0
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-05 11:18:07 +0000

    reading soil data

--- main.py
+++ main.py
@@ -8,18 +8,31 @@ for line in f:
     #reading seeds info
     line = line.strip()
     seeds = (line[line.find(":")+1:]).strip().split()
+    #line=[1,2,3] #making line not empty at the end of reading seeds
     print(seeds)
-    y = input("enter a key: ")
+    #y = input("enter a key at seeds: ")
 
-  elif count==1:
-    #reading soilmap info
-    line = line.strip()
-    soilMap = (line[line.find(":")+1:]).strip().split()
-    print(soi)
-    y = input("enter a key: ")
+  elif count==2:
+    soilMap=[]
+    print(f"line {line}")
+    y = input("enter a key at line")
+    while len(line)>0:
+      print(f"line {line}, length {len(line)}")
+      y = input("enter a key at while loop")
+      #reading soilmap info
+      line = str(f.readlines(1))
+      print(f"line {line}")
+      line = line.strip("[]\n'")
+      print(f"stripped line {line}")
+      soilMap.append((line[line.find(":")+1:-2]).strip().split())
+      print(soilMap)
+      #y = input("enter a key at soil: ")
+  elif count==4:
+    print(f"I am in count {count}")
+    y = input("enter a key at fert: ")
   
 
-
+  count+=1
 
 f.close
 
