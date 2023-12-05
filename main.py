a9dd6186a7413b4c9bde2683c40880e5bb5a4630
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-05 07:51:59 +0000

    read seeds input

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 
-f=open("day2_input.txt", "r")
+f=open("day5_input.txt", "r")
+
+#reading 8 inputs
+count = 0
+for line in f:
+  if count==0:
+    #reading seeds info
+    line = line.strip()
+    seeds = (line[line.find(":")+1:]).strip().split()
+    print(seeds)
+    y = input("enter a key: ")
+
+  elif count==1:
+    #reading soilmap info
+    line = line.strip()
+    soilMap = (line[line.find(":")+1:]).strip().split()
+    print(soi)
+    y = input("enter a key: ")
+  
 
 
 
