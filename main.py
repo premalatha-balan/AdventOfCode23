a2a32cb2d9290f163e4bdcb7fc5e4813a1c0e43c
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-06 17:01:26 +0000

    fixed all syntax mistakes

--- main.py
+++ main.py
@@ -8,44 +8,42 @@ def readFile2Data():
   
   seeds = inData[0].split()
   seeds.remove("seeds:")
-  seeds = map(int, seeds)
+  #print(f"seeds = {seeds} ")
+  #y = input("enter a key: ")
+  seeds = list(map(int, seeds))
+  #print(f"seeds = {seeds} ")
+  #y = input("enter a key: ")
   #print(seeds)
   seedPairs = [(seeds[i], seeds[i+1]) for i in range (0,len(seeds),2)]
-  
+  #print(f"seedPairs = {seedPairs}")
+  #y = input("enter a key: ")
   seed2SoilMap = inData[2:inData.index("soil-to-fertilizer map:")]
-  seed2SoilMap = [line.split() for line in seed2SoilMap]
-  seed2SoilMap = map(int, seed2SoilMap)
-  #print(seed2SoilMap)
+  seed2SoilMap = [list(map(int, line.split())) for line in seed2SoilMap]
+  #print(f"seed2SoilMap = {seed2SoilMap}")
+  #y = input("enter a key: ")
   
   soil2FertlMap = inData[inData.index("soil-to-fertilizer map:")+1:inData.index("fertilizer-to-water map:")]
-  soil2FertlMap = [line.split() for line in soil2FertlMap]
-  soil2FertlMap = map(int, soil2FertlMap)
+  soil2FertlMap = [list(map(int, line.split())) for line in soil2FertlMap]
   #print(soil2FertlMap)
   
   fertl2WaterMap = inData[inData.index("fertilizer-to-water map:")+1:inData.index("water-to-light map:")]
-  fertl2WaterMap = [line.split() for line in fertl2WaterMap]
-  fertl2WaterMap = map(int, fertl2WaterMap)
+  fertl2WaterMap = [list(map(int, line.split())) for line in fertl2WaterMap]
   #print(f"fertl2WaterMap = {fertl2WaterMap}")
   
   water2LightMap  = inData[inData.index("water-to-light map:")+1:inData.index("light-to-temperature map:")]
-  water2LightMap = [line.split() for line in water2LightMap]
-  water2LightMap = map(int, water2LightMap)
-  
+  water2LightMap = [list(map(int, line.split())) for line in water2LightMap]
   #print(f"water2LightMap = {water2LightMap}")
   
   light2TempMap = inData[inData.index("light-to-temperature map:")+1:inData.index("temperature-to-humidity map:")]
-  light2TempMap = [line.split() for line in light2TempMap]
-  light2TempMap = map(int, light2TempMap)
+  light2TempMap = [list(map(int, line.split())) for line in light2TempMap]
   #print(f"light2TempMap = {light2TempMap}")
   
   temp2HumidMap = inData[inData.index("temperature-to-humidity map:")+1:inData.index("humidity-to-location map:")]
-  temp2HumidMap = [line.split() for line in temp2HumidMap]
-  temp2HumidMap = map(int, temp2HumidMap)
+  temp2HumidMap = [list(map(int, line.split())) for line in temp2HumidMap]
   #print(f"temp2HumidMap = {temp2HumidMap}")
   
   humid2LocMap = inData[inData.index("humidity-to-location map:")+1:]
-  humid2LocMap = [line.split() for line in humid2LocMap]
-  humid2LocMap = map(int, humid2LocMap)
+  humid2LocMap = [list(map(int, line.split())) for line in humid2LocMap]
 
   #print(f"humid2LocMap = {humid2LocMap}")
 
@@ -67,36 +65,36 @@ def correspond(pair, destPair, sourcePair, mapData):
     if startPair>=start:
       if endPair<=end: # seed range is fully inside
         matched = xCorres(startPair, line[0], start, end)
-        destPair.append(matched, pair[1]) # tuple match is a soil correspondent tuple
+        destPair.append((matched, pair[1])) # tuple match is a soil correspondent tuple
         break
       else: #endPair>end:
         if startPair< end:
-          endMatch = line[2]+line[1]-matched #+1 & -1 cancel out
           matched = xCorres(startPair, line[0], start, end)
-          destPair.append(matched, endMatch)
-          sourcePair.append(endMatch+1,  endPair-end) #appending unmatched bottom bit as seed and range
+          endMatch = line[2]+line[1]-matched #+1 & -1 cancel out
+          destPair.append((matched, endMatch))
+          sourcePair.append((endMatch+1,  endPair-end)) #appending unmatched bottom bit as seed and range
           break
         elif startPair == end:
           endMatch==0
           matched = xCorres(startPair, line[0], start, end)
-          destPair.append(matched, 0)
+          destPair.append((matched, 0))
           break
         else: 
-          destPair.append(pair[0], pair[1])
+          destPair.append((pair[0], pair[1]))
           break
     else: #startPair<start
       if endPair<start:
-        destPair.append(pair[0], pair[1])
+        destPair.append((pair[0], pair[1]))
         break
-      elif endPair=start:
+      elif endPair==start:
         matched = xCorres(endPair, line[0],start, end)
-        destPair.append(matched, 0)
+        destPair.append((matched, 0))
         break
       else: #endPair>start:
-        sourcePair.append(startPair, startPair-start)
+        sourcePair.append((startPair, startPair-start))
         matched = xCorres(start, line[0], start, end)
         endMatch = endPair-start+1
-        destPair.append(matched, endMatch)
+        destPair.append((matched, endMatch))
         break
 
 
