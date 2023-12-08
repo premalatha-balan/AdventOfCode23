9b3f0b17a042c0f892b2af262bff0687da4e4ede
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-08 14:58:51 +0000

    The answer is not correct, but the code is efficient

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-"""
+
 def readFile2Data():
-  f=open("day5_input.txt", "r")
+  f=open("day6_input.txt", "r")
+  time = f[0].find(":").strip()
+  print(time)
   
  
   f.close
   return(data)
-"""
-#read this data from the file
+
+#read this data from the file removing all spaces and joining them up. Will do once I have got the solution. easy to do.
 #Time:        42     89     91     89
 #Distance:   308   1170   1291   1467
 
 
-getDist = lambda t: [(t-i)*i for i in range(1,int(t/2+1))] #getDist is a function
-
-print(getDist(42))
+#getDist = (lambda t, d: [(t-i)*i for i in range(1,int(t/2+1)) if (t-i)*i<d])  #getDist is a function
+readFile2Data
 y = input("enter a key: ")
 
-boatRace = lambda tupIn: len(list(filter(lambda a: True if a>tupIn[1] else False, getDist(tupIn[0]))))
+getDist = lambda t, d: sum((t - i) * i < d for i in range(1, int(t / 2) + 1))
 
-#data = [(42,308), (89,1170), (91,1291), (89,1467)] #part1
-data=(42899189, 308117012911467) #part2
+data=(42899189, 308117012911467) #part2 #
+
+ways = getDist(data[0], data[1])
+print(ways)
 
 #ways = list(map(boatRace, data)) #part 1
-ways = boatRace(data) #part 2
-print(ways)
+#ways = boatRace(data) #part 2
+#ways = len(getDist(data[0], data[1]))
+#print(ways)
 
 """result=1
 for i in range(len(ways)):
   result*=ways[i]
 print(result)"""
 
-
+"""
+getDist = lambda t, d: sum((t - i) * i < d for i in range(1, int(t / 2) + 1))
+data = (42899189, 308117012911467)
+ways = getDist(data[0], data[1])
+print(ways)"""
 
