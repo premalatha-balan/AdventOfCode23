48c5715b7aaca6e1be340ea2714e76eeb935d3df
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-08 21:30:37 +0000

    not doing well in defining types

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ def readFile2Data():
 
 #ile2DreadFata()
 
-hands[("32T3K", 765), ("T55J5", 684), ("KK677", 28), ("KTJJT", 220), ("QQQJA", 483)]
 
-type = {5: "fiveKind",
+types = {5: "fiveKind",
        4:"fourKind",
        3:"threeKind",
       32:"fullHouse",
@@ -15,24 +14,14 @@ type = {5: "fiveKind",
        1:"onePair",
        0:"highCard"}
 
-def types (hand):
-  if hand.count(i)==5 for i in hand:
-    type ="fiveKind"
-elif hand.count(i)==4 for i in hand:
-  type ="fourKind"
-elif hand.count(i)==2 for i in hand:
-  type ="twoPair"
-elif hand.count not > 1:
-  type ="highCard"
-elif hand.count(i)==3 for i in hand:
-  for i in set(hand):
-    if hand.count(i) == target_count:
-        result_string = i
-        break
-  if hand.count(i)==2 for i in hand:
-    type ="twoPair"
-  else:
-    type ="threeKind"
+def chkType (hand):
+  type = "fiveKind" if all(hand.count(i) == 5 for i in hand) else "fourKind"
+  type = "fourKind" if all(hand.count(i) == 4 for i in hand) else "threeKind"
+  type = "FullHouse" if all(hand.count(i) == 3 for i in hand) and if all(hand.count(i) == 2 for i in hand) else "threeKind"
+  type = "twoPair" if all(hand.count(i) == 2 for i in hand) and if all(hand.count(i) == 2 for i in hand) else "threeKind"
+  type = "onePair" if all(hand.count(i) == 2 for i in hand) and if all(hand.count(i) == 2 for i in hand) else "threeKind"
+
+  return type
 
 
 def winner(hand1, hand2):
@@ -42,3 +31,19 @@ def winner(hand1, hand2):
     return 2
   else:
     return 0
+
+hands=[("32T3K", 765), ("T55J5", 684), ("KK677", 28), ("KTJJT", 220), ("QQQJA", 483)]
+for hand in hands:
+  #print(f"{hand[0]} {hand[1]} {chkType(hand[0])} {chkType(hand[1])} {winner(hand[1],hand[0])}")  
+  print(hand, chkType(hand[0]))
+
+"""for i in hand:
+  if hand.count(i) == 3:
+    newstr = "".join([j for j in hand if j != i])
+    type = "fullHouse" if newstr.count(i) == 2 else "threeKind"
+  elif hand.count(i) == 2:
+    newstr = "".join([j for j in hand if j != i])
+    #type = "twoPair" if newstr.count(i) == 2 else "onePair"
+    print(newstr, i)
+  else:
+    type = "highCard""""
