83050b99158b88f7de15b35f6b46cbb18b3c22ed
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-08 19:44:23 +0000

    This needs more focus. leaving for now

--- main.py
+++ main.py
@@ -6,3 +6,39 @@ def readFile2Data():
 #ile2DreadFata()
 
 hands[("32T3K", 765), ("T55J5", 684), ("KK677", 28), ("KTJJT", 220), ("QQQJA", 483)]
+
+type = {5: "fiveKind",
+       4:"fourKind",
+       3:"threeKind",
+      32:"fullHouse",
+       2:"twoPair",
+       1:"onePair",
+       0:"highCard"}
+
+def types (hand):
+  if hand.count(i)==5 for i in hand:
+    type ="fiveKind"
+elif hand.count(i)==4 for i in hand:
+  type ="fourKind"
+elif hand.count(i)==2 for i in hand:
+  type ="twoPair"
+elif hand.count not > 1:
+  type ="highCard"
+elif hand.count(i)==3 for i in hand:
+  for i in set(hand):
+    if hand.count(i) == target_count:
+        result_string = i
+        break
+  if hand.count(i)==2 for i in hand:
+    type ="twoPair"
+  else:
+    type ="threeKind"
+
+
+def winner(hand1, hand2):
+  if hand1>hand2:
+    return 1
+  elif hand1<hand2:
+    return 2
+  else:
+    return 0
