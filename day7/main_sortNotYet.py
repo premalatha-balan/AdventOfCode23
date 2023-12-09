01eef06638fa4a4263e3dd2d8d3fdb1c96886a44
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-09 18:13:17 +0000

    saved the file that works but sorting not done yet

--- day7/main_sortNotYet.py
+++ day7/main_sortNotYet.py
@@ -52,22 +52,13 @@ def compare(hand1, hand2):
   for card1, card2 in zip(hand1, hand2):
     #print(f"card1 = {card1} and card2 = {card2} ")
     value1, value2 = card2value[card1], card2value[card2]
-    return True if value1 < value2 else False
+    if value1 > value2 : rank1+=1
+    elif value1 < value2 : rank2+=1
+    #print(f"rank1 = {rank1} and rank2 = {rank2} ")
+    if rank1> rank2 or rank2 > rank1: return rank1, rank2
+  return rank1, rank2
 
 def qSort(inList):
-  if len(inList) <= 1:
-    return inList
-  else:
-    pivot = inList[0]
-    left, right = [], []
-    for item in inList[1:]:
-      #if item < pivot:
-      if compare(item, pivot):
-        left.append(item)
-      else:
-        right.append(item)
-    return qsort(left) + [pivot] + qsort(right)
-    
   
 
 
@@ -103,8 +94,7 @@ secOrder = {
   t: [h for h, type_ in typesHands if type_ == t] 
   for t in types if any(type_ == t for _, type_ in typesHands)
 }
-print(f"secOrder {secOrder}")
-y = input("enter a key: ")
+#print(f"secOrder {secOrder}")
 
 
 rank=0
@@ -121,7 +111,24 @@ for type in secOrder:
   else:
     #print(type, secOrder[type])
    
+    for i in range(1, len(secOrder[type])):
+      print(type)
+      #print(f"calling {secOrder[type][i-1]} and {secOrder[type][i]} ")
+      rank1, rank2 = compare(secOrder[type][i-1], secOrder[type][i])
+      #print(f"{rank1} for {secOrder[type][i-1]} and {rank2} for {secOrder[type][i-1]}" )
 
+      if len(hands_dict[secOrder[type][i-1]])==1:
+        hands_dict[secOrder[type][i-1]].append(rank+rank1+1)
+      else:
+        hands_dict[secOrder[type][i-1]][1]=rank+rank1+1
+      if len(hands_dict[secOrder[type][i]])==1:
+        hands_dict[secOrder[type][i]].append(rank+rank2+1)
+      else:
+        hands_dict[secOrder[type][i]][1]=rank+rank2+1
+      rank=rank+rank1 if rank1>rank2 else rank+rank2
+      print(f" {secOrder[type][i-1]} and {secOrder[type][i]} ")
+      print(f" {hands_dict[secOrder[type][i-1]]}, {hands_dict[secOrder[type][i]]}")
+      y = input("enter a key: ")
 
 bids_rank = list(hands_dict.values())
 #print(bids_rank)
