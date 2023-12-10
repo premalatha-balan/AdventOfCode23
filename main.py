0d92817cb5e4db4aecec3e13f59a86effb188b1b
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-10 12:21:25 +0000

    god the diffLst

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-def readFile2Data():
-  f=open("day7_input.txt", "r")
-  hands_dict={}
-  hands_dict = {(line.strip().split())[0]: [int((line.strip().split())[1])] for line in f}
-  f.close
-  return(hands_dict)
+import copy
 
+def readFile2Data():
+  f=open("day9_input.txt", "r")
+  #for line in f:
+    #line = line.strip().split()
+    #print(line)
+    #y = input("enter a key: ")
+  seqBigLst = [line.strip().split() for line in f]
+  seqBigLst = [list(map(int, seq)) for seq in seqBigLst]
+  
+  f.close
+  return(seqBigLst)
+
+def diff(lst):
+  return [abs(lst[i]-lst[i-1]) for i in range(1, len(lst))]
+
+def get_diffLst(lst):
+  #print(lst)
+  diffLst = [[]]
+  diffLst[0] = copy.deepcopy(lst)
+  #print(diffLst)
+  while (any(lst) and len(lst)>1):
+    lst = diff(lst)
+    #print(lst)
+    #y = input("enter a key: ")
+    diffLst.append(lst)
+    #print(diffLst)
+    #y = input("enter a key: ")
+  return diffLst
+
+seqBigLst = readFile2Data()
+for line in seqBigLst:
+  diffLst = get_diffLst(line)
+  print(f"{diffLst} outside the function")
+  y = input("enter a key: ")
+  diffLst = [l+[0] for l in diffLst]
+  #diffLst = list(map(_.append(0), diffLst))
+  
+  print(f"{diffLst} after appending")
+  y = input("enter a key: ")
