d755f671b5a329170d759ce54aed37619e1d4673
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-12 12:39:34 +0000

    Not happy with this code either

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def readFile2Data():
   f.close
   return(instructions, data)
 
+
 def build_graph(data):
     G = nx.DiGraph()
     for node, moves in data.items():
@@ -28,32 +29,18 @@ def build_graph(data):
 def perform_moves(graph, start_nodes, instructions):
     current_nodes = set(start_nodes)
     step = 0
-    #print(f"current nodes = {current_nodes} at {step} steps ")
-    
 
     while True:
         i = instructions[step]
-        next = 0 if i == "L" else 1
-      
-        new_nodes = set()
-        for node in current_nodes:
-            neighbors = list(graph.successors(node))
-            #print(f"{node} => {neighbors} ")
-            #y = input("enter a key: ")
-            if neighbors:
-              if len(neighbors)==2:
-                #print(f"neighbours {neighbors} ")
-                new_nodes.add(neighbors[next])
-                #print(f"new_nodes {new_nodes} ")
-                #y = input("enter a key: ")
-              elif len(neighbors)==1:
-                neighbors.append(neighbors[0])
-                #print(f"neighbours {neighbors} ")
-                new_nodes.add(neighbors[next])
-                #print(f"new_nodes {new_nodes} ")
-                #y = input("enter a key: ")
+        next_move = 0 if i == "L" else 1
 
-        current_nodes = new_nodes
+        new_nodes = [
+            neighbor
+            for node in current_nodes
+            for neighbor in graph.successors(node)
+        ]
+
+        current_nodes = set(new_nodes)
         step += 1
 
         if all(node[2] == "Z" for node in current_nodes):
@@ -64,10 +51,9 @@ def perform_moves(graph, start_nodes, instructions):
     return step
 
 instructions, data = readFile2Data()
-#print(data.items())
-#y=input("enter a key: ")
 graph = build_graph(data)
-start_nodes = [node for node in data if node[2] == "A"]
+start_nodes = {node for node in data if node[2] == "A"}
 
 result = perform_moves(graph, start_nodes, instructions)
 print(result)
+
