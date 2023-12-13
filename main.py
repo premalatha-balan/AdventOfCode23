5635f7147d23fbc2a84565338a523cb56f49543b
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-13 12:33:26 +0000

    some bonkers going on

--- main.py
+++ main.py
@@ -42,29 +42,39 @@ while True:
     if length<step+step:
       instructions=instructions*2
       length = len(instructions)
-    for i in range(0,step):
-      dir = instructions[i]
-      if dir == instructions[step+i]:
-        print(neighbours_lst[i], neighbours_lst[step+i], i)
-
-        y = input("enter a key: ")
-        match_move = i 
+    for i in range(match_posn,step):
+      dirl = instructions[i]
+      print(f"dir = {dirl} and instructions[step+i] = {instructions[step+i]} at i = {i} step {step+i} ")
+      y = input("enter a key: ")
+      if dirl == instructions[step+i]:
+        match_move = i+1
+        print(f"match_move = {match_move}")
+        break
       else:
         break
 
     print(node)
-    node = node_lst[match_move]
     print(f"match move {match_move}, node = {node} ")
+    print(f"match_posn {match_posn} ")
+    y = input("enter a key: ")
+    for i in range(match_posn,match_move+1):
+      path.append(path[i])
+    node_posn = neighbours_lst.index(path[-1])
+    node = node_lst[node_posn]
+    dir = instructions[step+match_move]
+    next = 0 if dir == "L" else 1
+    neighbour = neighbours_lst[node_posn]
+    step+=(match_move+1)
+    print(f"node = {node}, neighbhour = {neighbour} ")
+    print(f"path= {path}" )
     y = input("enter a key: ")
     
-        
-        
-
-  path.append(neighbour)
-  #print(f"path {path}")
-  next = 0 if dir == "L" else 1
-  node = data[node][next]
-  step+=1
+  else:
+    path.append(neighbour)
+    step+=1
+    next = 0 if dir == "L" else 1
+    node = data[node][next]
+  
   if step==length-2:
     instructions=instructions*2
     length=len(instructions)
