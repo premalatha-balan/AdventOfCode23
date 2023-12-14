850ebb1c55153ba438bbda31a170f4c585bd7131
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-14 16:16:06 +0000

    finally doing some analysis of the pattern and math outside the code helped

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import copy
+import math
 
 def readFile2Data():
   f=open("day8_input.txt", "r")
@@ -15,6 +17,7 @@ def readFile2Data():
   f.close
   return(instructions, data)
 
+
 instructions, data = readFile2Data()
 instr_loc_tpl = list(enumerate(instructions))
 """fi = open("instructions.txt", "w")
@@ -29,86 +32,62 @@ node_starts = np.array([node for node in node_lst if node[2] == "A"])
 print(node_starts)
 #print(neighbours_lst)
 #y = input("enter a key")
-
-#initialising all things
-step = 0
-length = len(instructions)
-node=node_starts[0]
-dir = instructions[0]
-next = 0 if dir == "L" else 1
-neighbour = data[node]
-path=[(neighbour)]
-nodesInPath = np.array(node)
-#print(path)
-matches = {}
-while node[2]!="Z" and step<length:
-  #print(f"inside the while loop with node {node}")
-  #moving now
-  next = 0 if dir =="L" else 1
-  node = data[node][next] #this will give which node it should go next such as QKS for XSA (QKS, MND) and L 
-  
-  #this is to append everything
-  step+=1
+travel = []
+o_length = len(instructions)
+for n in range(len(node_starts)):
+  #initialising all things
+  step = 0
+  length = len(instructions)
+  #print(f"length = {length}")
+  node=node_starts[n]
+  dir = instructions[0]
+  next = 0 if dir == "L" else 1
   neighbour = data[node]
-
-  if neighbour in path: # there is a match
-    #print(f"neighbour {neighbour} is in path {path} ")
-    #y = input("enter a key: ")
+  #path=[(neighbour)]
+  #nodesInPath = np.array(node)
+  #print(path)
+  #matches = {}
+  while node[2]!="Z" and step<length:
+    #print(f"inside the while loop with node {node}")
+    #moving now
+    next = 0 if dir =="L" else 1
+    node = data[node][next] #this will give which node it should go next such as QKS for XSA (QKS, MND) and L 
     
-    # doing the usual move
-    #dir = instructions[step]
-    path.append((neighbour))
-    nodesInPath = np.append(nodesInPath, (node))
-    if neighbour not in matches: #first time finding the match
-      initial = path.index(neighbour)
-      matches.update({neighbour:[initial, step]}) 
-      dir_now = instructions[initial:step]
-      
-      #making sure the length of the instructions is enough
-      if length<step*2-initial:
-        instructions=instructions*2
-        length = len(instructions)
-      #checking out if there is branch in the  previously matched ones which we can take and add to the path
-      for i in range(1,len(dir_now)):
-        l = dir_now[i]
-        r = instructions[step+1+i]
-        if l == r:
-          #this is worth cutting a branch even though there are only two next matches. 
-          neighbour = path[i]
-          path.append(neighbour)
-          nodesInPath = np.append(nodesInPath, (nodesInPath[i]))
-          print(f"checking out at {i} and {step+1}")
-          print(f"neighbour {neighbour} and node {node}")
-          y = input("enter a key: ")
-        else:
-          branch = []
-          print(f"don't bother at {i} and {step+1}")
-          y = input("enter a key: ")
-      
-      #working here
-    else: #print(f"Yes, there are more than one matches at {neighbour} at {step} ")
-      matches[neighbour].append(step)
-    print(matches)
-    y = input("enter a key: ")
-
-  else: #neighbour not in path no match. so continue as usual
+    #this is to append everything
+    step+=1
+    neighbour = data[node]
+  
+    #decided to do the usual way as there is not a lot of gain in cutting the branches. maybe, but leaving it for now.
     dir = instructions[step]
-    path.append((neighbour))
-    nodesInPath = np.append(nodesInPath, (node))
+    #path.append((neighbour))
+    #nodesInPath = np.append(nodesInPath, (node))
     #print(f"path = {path}")
     #print(f"step = {step} ")
     #print(f"node = {node}")
     #y = input("enter a key: ")
+  
+    if step == length-1:
+      #print(f"inside if checking length {length} ")
+      instructions=instructions*2
+      length=len(instructions)
+      #print(f"length = {length}")
+      #y = input("enter a key: ")
+  
+  #print(path)
+  #print(step, n)
+  #travel.append((node_starts[n], node, step))
+  travel.append(step)
+  #y = input("enter a key: ")
+#print(f"length = {length}")
+print(travel)
 
-  if step == length-1:
-    #print(f"inside if checking length {length} ")
-    instructions=instructions*2
-    length=len(instructions)
-    #print(f"length = {length}")
-    #y = input("enter a key: ")
+#travel.sort()
+#print(travel)
 
-#print(path)
-#print(step+1)
+result = int(math.prod(travel)/(o_length**5))
+print(result)
+print(5**2)
+
 
 """fw = open("path.txt", "w")
 for tpl in path:
