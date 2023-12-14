a4f4c793d9053b0aa22cb38f53911600a3967f4f
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-14 12:18:04 +0000

    can find matches. Checking for branches

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def readFile2Data():
   return(instructions, data)
 
 instructions, data = readFile2Data()
+instr_loc_tpl = list(enumerate(instructions))
+"""fi = open("instructions.txt", "w")
+for i in instr_loc_tpl:
+  tplStr = str(i)
+  fi.write(tplStr)
+  fi.write("\n")
+fi.close()"""
 node_lst = list(data.keys())
 #neighbours_lst = list(data.values())
 node_starts = np.array([node for node in node_lst if node[2] == "A"])
@@ -45,11 +52,32 @@ while node[2]!="Z" and step<length:
   neighbour = data[node]
 
   if neighbour in path:
-    print(f"neighbour {neighbour} is in path {path} ")
-    y = input("enter a key: ")
+    #print(f"neighbour {neighbour} is in path {path} ")
+    #y = input("enter a key: ")
     if neighbour not in matches:
-      matches.update{neighbour:[path.index(neighbour), step]} #working here
+      initial = path.index(neighbour)
+      matches.update({neighbour:[initial, step]}) 
+      dir_now = instructions[initial:step]
+      #making sure the length of the instructions is enough
+      if length<step*2-initial:
+        instructions=instructions*2
+        length = len(instructions)
+      #checking out if there is branch in the  previously matched ones which we can take and add to the path
+      for i in range(1,len(dir_now)):
+        l = dir_now[i]
+        r = instructions[step+1]
+        if l == r:
+          print(f"checking out at {i} and {step+1}")
+          #this is worth cutting a branch and adding it although only two here. Prefer to do more than two.
+          y = input("enter a key: ")
+        else:
+          print(f"don't bother at {i} and {step+1}")
+          y = input("enter a key: ")
+                
+        
+      #working here
     else:
+      print(f"Yes, there are more than one matches at {neighbour} at {step} ")
       matches[neighbour].append(step)
     print(matches)
     y = input("enter a key: ")
