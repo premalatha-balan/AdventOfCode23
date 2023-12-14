46e068c05c36c2a30f4d119372b2d29dcb34feb2
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-14 09:54:51 +0000

    fresh start

--- main.py
+++ main.py
@@ -17,8 +17,65 @@ def readFile2Data():
 
 instructions, data = readFile2Data()
 node_lst = list(data.keys())
-neighbours_lst = list(data.values())
+#neighbours_lst = list(data.values())
 node_starts = np.array([node for node in node_lst if node[2] == "A"])
 print(node_starts)
+#print(neighbours_lst)
+#y = input("enter a key")
 
-#ends = np.array[node for node in node_lst if node[2] == "Z"]
+#initialising all things
+step = 0
+length = len(instructions)
+node=node_starts[0]
+dir = instructions[0]
+next = 0 if dir == "L" else 1
+neighbour = data[node]
+path=[(neighbour)]
+nodesInPath = np.array(node)
+#print(path)
+matches = {}
+while node[2]!="Z" and step<length:
+  #print(f"inside the while loop with node {node}")
+  #moving now
+  next = 0 if dir =="L" else 1
+  node = data[node][next] #this will give which node it should go next such as QKS for XSA (QKS, MND) and L 
+  
+  #this is to append everything
+  step+=1
+  neighbour = data[node]
+
+  if neighbour in path:
+    print(f"neighbour {neighbour} is in path {path} ")
+    y = input("enter a key: ")
+    if neighbour not in matches:
+      matches.update{neighbour:[path.index(neighbour), step]} #working here
+    else:
+      matches[neighbour].append(step)
+    print(matches)
+    y = input("enter a key: ")
+  
+  dir = instructions[step]
+  path.append((neighbour))
+  nodesInPath = np.append(nodesInPath, (node))
+  #print(f"path = {path}")
+  #print(f"step = {step} ")
+  #print(f"node = {node}")
+  #y = input("enter a key: ")
+
+  if step == length-1:
+    #print(f"inside if checking length {length} ")
+    instructions=instructions*2
+    length=len(instructions)
+    #print(f"length = {length}")
+    #y = input("enter a key: ")
+
+#print(path)
+#print(step+1)
+
+"""fw = open("path.txt", "w")
+for tpl in path:
+  tplStr = str(tpl)
+  fw.write(tplStr)
+  fw.write("\n")
+fw.write(str(step+1))
+fw.close"""
