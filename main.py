f071b269afd25691f5ac024d0d11d4eca34c48a1
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-15 07:33:45 +0000

    reading file to np.array done

--- main.py
+++ main.py
@@ -3,12 +3,30 @@ import copy
 import math
 
 def readFile2Data():
-  f=open("day8_input.txt", "r")
+  f=open("day10_input.txt", "r")
+  tile_row = []
+  tile2Dlst =[[]]
+  tiles=np.array([])
+  #tiles = np.array(tile2Dlst)
+  count = 0
   for line in f:
     line=line.strip().split()
-    print(line)
-    y=input("enter a key: ")
-
+    for i in line:
+      tile_row = [c for c in i]
+      tiles = np.r_[tiles,tile_row]
+      #tiles = np.append(tiles,tile_row) #does the same too
+      count+=1
+  length = len(tile_row)
+  tiles = tiles.reshape(count, length)
+  #print(tiles.shape)
+  #print(tile2Dlst.shape)
+  #tile2Dlst = np.array(tile2Dlst)
   f.close
-  return(instructions, data)
+  return tiles
+
+tiles = readFile2Data()
+#print(len(tiles))
+print(tiles.shape)
+#print(tiles)
+#print(tiles[-1])
 
