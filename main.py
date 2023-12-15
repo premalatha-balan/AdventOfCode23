b4fbbbc3d6e24cb48b09804cff223788a4ca1324
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-15 08:29:16 +0000

    we can get neighbhours of the start "S"

--- main.py
+++ main.py
@@ -24,9 +24,33 @@ def readFile2Data():
   f.close
   return tiles
 
-tiles = readFile2Data()
-#print(len(tiles))
-print(tiles.shape)
-#print(tiles)
-#print(tiles[-1])
 
+def check_bounds(y,x, r, c):
+  if y+1>=r  or y-1<0 or x+1>=c or x-1<0 : return False
+  else: return True
+
+
+def get_neigh(el,y,x):
+  neigh=np.array([[tiles[y-1][x-1],tiles[y-1][x],tiles[y-1][x+1]], [tiles[y][x-1],tiles[y][x],tiles[y][x+1]], [tiles[y+1][x-1],tiles[y+1][x],tiles[y+1][x]]])
+  return neigh
+
+def get_move(neigh):
+  
+  
+  moveL, moveR = -1, +1 if "-": else 0, 0
+  moveU, moveD = -1, +1 if "|": else 0, 0
+  #moveU, moveL = 1, -1 if "L": else 0, 0
+    
+    
+
+tiles = readFile2Data()
+r, c = tiles.shape
+print(r, c)
+
+y, x = np.where(tiles == "S")
+y,x=y[0],x[0]
+print(x, y)
+
+
+neigh = get_neigh(tiles[y,x],y,x) if check_bounds(y,x,r,c) else None
+print(neigh)
