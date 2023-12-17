3d00d6b7dc1bf6995b66eef5d767ccc62aac3384
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-17 16:37:37 +0000

    read file into data

--- main.py
+++ main.py
@@ -5,25 +5,35 @@ import math
 
 def readFile2Data():
   f=open("testdataday12.txt", "r")
-  springNum, springMap =[[]],[[]]
+  springNum, springMap =[],[]
   numstr = [i for i in range(9)]
+  count = 0
   for line in f:
-    line = line.strip()
-    for i in line:
-      print(f"line = {line}")
-      row = [c for c in line if c=="." or c=="#" or c=="?"] #the handle has moved to the end. so nothing there to extract from the line
-      print(f"row = {row}")
-      z = input("enter a key: ")
-      springMap.append(row)
-      row = [int(c) for c in line if c in numstr]
-      print(f"row = {row}")
-      z = input("enter a key: ")
-      springNum.append(row)
+    line = line.strip("")
+    #print(f"line = line")
+    start = max(line.rfind("#"), line.rfind("?"), line.rfind("."))
+    row = line[start+1: ].strip().split(",")
+    row = [int(r) for r in row]
+    springNum.append(row)
+    #print(f"springNum = {springNum}")
+    #z = input("enter a key: ")
+      
+    row = [line[c] for c in range(start+1)]
+    #print(f"row = {row}")
+    #z = input("enter a key: ")
+    springMap.append(row)
+    #print(f"springMap = {springMap}")
+    #z = input("enter a key: ")
   f.close
   return springMap, springNum
 
 
 springMap, springNum = readFile2Data()
+
+print(len(springMap))
+print(len(springNum))
+z = input("enter a key: ")
+
 for i in range(len(springMap)):
   print(f"{springMap[i]}, {springNum[i]}")
   z = input("enter a key: ")
