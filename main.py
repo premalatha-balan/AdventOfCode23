8ed5eb94b3bdf697fc292a9af9e51d194d60a1e0
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-17 08:24:05 +0000

    not there with part2 yet

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from itertools import combinations
 import math
 
 def readFile2Data():
-  f=open("day11_input.txt", "r")
+  f=open("testdata2day11.txt", "r")
   image=np.array([])
   count=0
   for line in f:
@@ -36,6 +36,8 @@ def dist_pair(pair):
   gal1 = galaxies[pair[0]][0]
   gal2 = galaxies[pair[1]][0]
   #print(gal1, gal2)
+  dist = abs(gal1[0]-gal2[0])+abs(gal1[1]-gal2[1])
+  print(f"gal1 = {gal1}, gal2 = {gal2} and  dist = {dist}")
   #z= input("enter a key: ")
   return abs(gal1[0]-gal2[0])+abs(gal1[1]-gal2[1])
   
@@ -49,21 +51,21 @@ gal_wh = np.where(image == "#")
 #print(len(gal_wh))
 #print(f"length of gal_wh[0] = {len(gal_wh[0])}")
 #print(f"length of gal_wh[1] = {len(gal_wh[1])}")
-#print(f"gal_wh[0] before expanding {gal_wh[0]}")
+print(f"gal_wh[0] before expanding {gal_wh[0]}")
 #print(f"gal_wh[1] before expanding {gal_wh[1]}")
 #print(len(gal_wh[1]))
 #z= input("enter a key at checking: ")
 
-#checking the rows where there are no galaxies so that we can expand that row
+"""#checking the rows where there are no galaxies so that we can expand that row
 row_insert = ["." for i in range(c)]
 count=0
 for i in range(r):
   if i not in gal_wh[0]:
-    #print(f"row {i} is not a galaxy")
+    print(f"row {i} is not a galaxy")
     image = np.insert(image, i+count, row_insert, axis=0)
     count+=1
 
-#print(f"added {count} rows")
+print(f"added {count} rows")
 #print(f"after expanding rows, image shape is {image.shape} ")
 #z= input(f"enter a key after expanding rows: {r+count} ")
 r,c = image.shape
@@ -73,27 +75,27 @@ column_insert = ["." for i in range(r)]
 count=0
 for i in range(c):
   if i not in gal_wh[1]:
-    #print(f"column {i} is not a galaxy")
+    print(f"column {i} is not a galaxy")
     image = np.insert(image, i+count, column_insert, axis=1)
     count+=1
 
-#print(f"added {count} columns")
+print(f"added {count} columns")
 
-r,c = image.shape
+r,c = image.shape"""
 #print(f"after expanding rows, image shape is {image.shape} ")
 #z= input(f"enter a key after expanding columns: ")
 
 #print(f"image =/n/n {image}")
 
-#finding the galaxies new locations after the expansion
+"""#finding the galaxies new locations after the expansion
 #getting all the galaxies
 gal_wh = np.where(image == "#")
 #print(len(gal_wh))
-#print(f"after expanding")
+print(f"\n\nafter expanding")
 #print(f"length of gal_wh[0] = {len(gal_wh[0])}")
 #print(f"length of gal_wh[1] = {len(gal_wh[1])}")
-#print(f"gal_wh[0] after expanding {gal_wh[0]}")
-#print(f"gal_wh[1] after expanding {gal_wh[1]}")
+print(f"gal_wh[0] after expanding {gal_wh[0]}")
+print(f"gal_wh[1] after expanding {gal_wh[1]}")
 #print(len(gal_wh[1]))
 #z= input("enter a key at checking: ")
 
@@ -101,12 +103,73 @@ gal_wh = np.where(image == "#")
 galaxies = np.array([[(gal_wh[0][i], gal_wh[1][i])] for i in range(len(gal_wh[0]))])
 #print(galaxies.shape)
 #print(f"{galaxies[0][0][0]}, {galaxies[0][0][1]}")
+#print(len(galaxies))"""
+
+#expanding 10 times
+expand=0
+count=0
+times=1
+#print("\n\nworking on rows")
+for i in range(r):
+  if i not in gal_wh[0]:
+    #print(f"row {i} is not a galaxy")
+    #image = np.insert(image, i+count, row_insert, axis=0)
+    expand+=times
+    #z= input("enter a key: ")
+    #gal_wh[0] = np.insert(gal_wh[0], (gal_wh[0].index[i]) )
+    count+=1
+  elif count!=0:
+    loc = list(np.where(gal_wh[0]==i))
+    for l in range(len(loc)):
+      print(f"changing {gal_wh[0][loc[l]]} to {gal_wh[0][loc[l]]+expand}")
+      gal_wh[0][loc[l]] = gal_wh[0][loc[l]]+expand
+      print(f"changed {gal_wh[0][loc[l]]}")
+      #z= input("enter a key: ")
+
+print(f"after expanding rows, gal_wh[0] is {gal_wh[0]}")
+#z= input("enter a key: ")
+#print(f"added {count} rows")
+
+
+count=0
+expand=0
+times=1
+#print("\n\nworking on columns")
+print(f"gal_wh[1] before expanding {gal_wh[1]}")
+for i in range(c):
+  if i not in gal_wh[1]:
+    #print(f"column {i} is not a galaxy")
+    #image = np.insert(image, i+count, row_insert, axis=0)
+    expand+=times
+    #print(f"expand = {expand} and count = {count}")
+    #z= input("enter a key: ")
+    #gal_wh[0] = np.insert(gal_wh[0], (gal_wh[0].index[i]) )
+    count+=1
+  elif count!=0:
+    loc = list(np.where(gal_wh[1]==i))
+    for l in range(len(loc)):
+      #print(f"changing {gal_wh[1][loc[l]]} to {gal_wh[1][loc[l]]+expand}")
+      gal_wh[1][loc[l]] = gal_wh[1][loc[l]]+expand
+      #print(f"changed {gal_wh[1][loc[l]]}")
+      #print(f"expand = {expand} and count = {count}")
+      #z= input("enter a key: ")
+
+print(f"after expanding columns, gal_wh[1] is {gal_wh[1]}")
+#z= input("enter a key: ")
+
+#creating the galaxies pairs after expanding
+galaxies = np.array([[(gal_wh[0][i], gal_wh[1][i])] for i in range(len(gal_wh[0]))])
+#print(galaxies.shape)
+#print(f"{galaxies[0][0][0]}, {galaxies[0][0][1]}")
 #print(len(galaxies))
+#print(galaxies)
 
 #creating the combination of pairs with their ids
 ids = np.array([i for i in range(len(gal_wh[0]))])
 pairs = list(combinations(ids, 2))
 
 sum_lst = list(map(dist_pair, pairs))
+sum_lst.sort()
+print(f"sume of distances = {(sum_lst)}")
 print(f"sum = {sum(sum_lst)}")
 
