28d2476255036ad1b0d2e69c2be97b360ce8c18a
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-18 06:58:49 +0000

    planning to use string manipulation and pattern search instead of numpy.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import numpy as np
+#import numpy as np
 from itertools import combinations
 import math
 
 
 def readFile2Data():
   f=open("testdataday12.txt", "r")
-  springNum, springMap =[],[]
-  numstr = [i for i in range(9)]
+  springNum =[]
+  springMap=[]
   count = 0
   for line in f:
     line = line.strip("")
@@ -17,8 +17,7 @@ def readFile2Data():
     springNum.append(row)
     #print(f"springNum = {springNum}")
     #z = input("enter a key: ")
-      
-    row = [line[c] for c in range(start+1)]
+    row =line[:start+1].strip()
     #print(f"row = {row}")
     #z = input("enter a key: ")
     springMap.append(row)
@@ -29,14 +28,22 @@ def readFile2Data():
 
 
 springMap, springNum = readFile2Data()
+print(springMap)
 
-print(len(springMap))
-print(len(springNum))
-z = input("enter a key: ")
-
-for i in range(len(springMap)):
-  print(f"{springMap[i]}, {springNum[i]}")
-  z = input("enter a key: ")
+length = len(springMap)
+#changing the lists to np.arrays so that I can use where function to find where the ?s are and where 1s are etc. 
+"""for i in range(length):
+  springMap[i] = np.array(springMap[i])
+  springNum[i] = np.array(springNum[i])
+  xq = np.array(np.where(springMap[i] == "?"))[0]
+  xh = np.array(np.where(springMap[i] == "#"))[0]
+  xd = np.array(np.where(springMap[i] == "."))[0]
+  print(xq, xh, xd)
+  print(springNum[i])
+  z = input("enter a key: ")"""
+  
+  
+  
   
 
 
