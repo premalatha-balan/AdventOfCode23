b7bf3ef199d3888e8f5f88baff35542159be97f8
premalatha-balan <premalatha_balan@yahoo.co.uk>
2023-12-22 19:47:24 +0000

    Can find mirror in rows

--- main.py
+++ main.py
@@ -1,25 +1,76 @@
+import numpy as np
 
-f=open("day12_input.txt", "r")
+f=open("testdataday13.txt", "r")
 
-ways = 0
-for row in f:
-    record, checksum = row.split()
+def readFile2Data(f):
+  data=[]
+  for line in f:
+    line=line.strip()
+    if len(line)!=0:
+      data.append(line.strip())
+    else:
+      return data
+  return data
 
-    checksum = [int(n) for n in checksum.split(',')]
-    record = '?'.join([record for i in range(5)])
-    checksum *= 5
-    #print(f"record = {record}, checksum = {checksum}")
-    #z = input("enter a key: ")
-    positions = {0: 1}
-    for i, contiguous in enumerate(checksum):
-        new_positions = {}
-        for k, v in positions.items():
-            for n in range(k, len(record) - sum(checksum[i + 1:]) + len(checksum[i + 1:])):
-                if n + contiguous - 1 < len(record) and '.' not in record[n:n + contiguous]:
-                    if (i == len(checksum) - 1 and '#' not in record[n + contiguous:]) or (i < len(checksum) - 1 and n + contiguous < len(record) and record[n + contiguous] != '#'):
-                        new_positions[n + contiguous + 1] = new_positions[n + contiguous + 1] + v if n + contiguous + 1 in new_positions else v
-                if record[n] == '#':
-                    break
-        positions = new_positions
-    ways += sum(positions.values())
-print(ways)
+def findMirror():
+  length = len(data)
+  print(f"length = {length}")
+  for m in range(1,length):
+    print(data[m])
+    if data[m-1]==data[m]:
+      print(f"found mirror between {m-1} and {m} ")
+      z = input("enter a key: ")
+      return m
+  return False
+
+def findRefln(m):  
+  print(m-1, m)
+  up = data[:m] 
+  print(up)
+  up = up[::-1]
+  print(up)
+  down = data[m:]
+  small = min (len(up), len(down))
+  print(f" small = {small}")
+  count = 0
+  for j in range(small):
+    if up[j] == down[j]:
+      count+=1
+    else: 
+      print("not mirror")
+      return False
+  z = input("enter a key: ")
+  return count
+  
+
+
+while True:
+  data = readFile2Data(f)
+
+  m = findMirror()
+  print(f"m = {m}")
+  z = input("enter a key: ")
+  if m:
+    count = findRefln(m)
+    print(m, count)
+    z = input("enter a key: ")
+  
+  """data = np.array([list(line) for line in data])
+  print(data)
+  z = input("enter a key: ")
+  r,c = data.shape
+  for i in range(1,r):
+    if "".join(data[i-1]) == data[i]:
+      print(f"found mirror between {i-1} and {i+1} ")
+      z = input("enter a key: ")
+  """
+  
+  
+
+  
+  if len(data)==0:
+    print("reached the end of file")
+    break
+#print(data)
+    
+f.close()
